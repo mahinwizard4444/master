e0b0a55d3b14e8ac560478acae677d632a935d6c
CLÆY <78960870+CLaY9950@users.noreply.github.com>
2021-08-30 14:11:09 +0530

    Update filters_mdb.py

--- plugins/filters_mdb.py
+++ plugins/filters_mdb.py
@@ -3,12 +3,12 @@ import re
 import pymongo
 
 if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
+    from sample_info import Config
 else:
     from config import Config
  
-myclient = pymongo.MongoClient(Config.DATABASE_URI)
-mydb = myclient[Config.DATABASE_NAME]
+myclient = pymongo.MongoClient(Config.DB_URI)
+mydb = myclient[Config.DB_NAME]
 
 
 
