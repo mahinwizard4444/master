d9918749b99f9697e2d2ca59f5bd39ad11b9a471
NoDroidBots <90566330+NoDroidBots@users.noreply.github.com>
2021-09-14 16:57:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ class Bot(Client):
         await Media.ensure_indexes()
         me = await self.get_me()
         self.username = '@' + me.username
-        print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}. By NoDroid Bots ✓")
 
     async def stop(self, *args):
         await super().stop()
